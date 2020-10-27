81c4d08204e75d53534d5be54a683709732600e8
weidonglian <weidonglian@users.noreply.github.com>
2020-10-27 22:14:05 +0000

    deploy: 7d681c227ec1bb78be2fb9a5308c0dec9bbd25f9

---
+++ precache-manifest.74bca117d2f4818d9ef81afda856b159.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9295ddafda2e6efee08633210672a04d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3c00730012045dd0e806",
+    "url": "./static/js/2.2c2a0d29.chunk.js"
+  },
+  {
+    "revision": "44d582a97d02337b347d751d7baba8fd",
+    "url": "./static/js/2.2c2a0d29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b40875bc8c3a60a267ec",
+    "url": "./static/js/main.05614478.chunk.js"
+  },
+  {
+    "revision": "5436d20169492f71619c",
+    "url": "./static/js/runtime-main.e4fd56eb.js"
+  }
+]);
